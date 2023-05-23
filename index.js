aaf5a706447865dc52c770cfd0706c2dc522b4f7
Ikem Krueger <ikem.krueger@gmail.com>
2023-05-23 20:46:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function utc2unix(string) {
+  return Date.parse(string);
+}
+
+function unix2utc(timestamp) {
+  return new Date(new Number(timestamp)).toUTCString();
+}
+
 // index.js
 // where your node app starts
 
@@ -18,13 +26,41 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api", function (req, res) {
+  const date = new Date();
+
+  res.json({ unix: utc2unix(date.toUTCString()), utc: date.toUTCString() });
+});
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date", function (req, res) {
+  const _date = req.params.date;
 
+  const is_unix_timestamp = /^\d{1,13}$/.test(_date);
+  const is_utc_string = /^\d{4}(-\d\d(-\d\d(T\d\d:\d\d(:\d\d)?(\.\d+)?(([+-]\d\d:\d\d)|Z)?)?)?)?$/.test(_date);
+
+  // ECMA Script Date Time String Format:
+  // https://tc39.es/ecma262/multipage/numbers-and-dates.html#sec-date-time-string-format
+
+  // RegEx copied from here:
+  // https://stackoverflow.com/questions/12756159/regex-and-iso8601-formatted-datetime/37563868#37563868
+
+  let date = { error: "Invalid Date" };
+
+  if (is_unix_timestamp) {
+    date = { unix: new Number(_date), utc: unix2utc(_date) };
+  }
+
+  if (is_utc_string) {
+    date = { unix: utc2unix(_date), utc: unix2utc(utc2unix(_date)) };
+  }
+
+  res.json(date);
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
